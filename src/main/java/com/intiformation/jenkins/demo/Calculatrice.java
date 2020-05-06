4b5244d4dfebe462fd0802fa8a01dee2950bd21d
Marie Tulliez <marie-tulliez@hotmail.fr>
2020-05-06 09:43:32 +0200

    02 jenkins

---
+++ src/main/java/com/intiformation/jenkins/demo/Calculatrice.java
@@ -0,0 +1,30 @@
+package com.intiformation.jenkins.demo;
+
+/**
+ * 
+ * @author IN-MP-039
+ *
+ */
+public class Calculatrice {
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int add (int a, int b) {
+		return a+b;
+	}//end add
+	
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int mul (int a, int b) {
+		return a*b;
+	}//end mul
+	
+}//end class
